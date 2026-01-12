68ebc362e18b5f3952845871805c7c56126429bd
Explosif3005 <75031787+Explosif3005@users.noreply.github.com>
2026-01-12 14:06:48 +0100

    Update APIfonctions.py
    Change of update fonction to request_data + generalisation of fonction + add change the name of 'data.json' from 'data.json' to f"data_{name}.json" (where {name} is the str parameters 'name')

--- APIfonctions.py
+++ APIfonctions.py
@@ -1,16 +1,16 @@
 #!/bin/python
 """
-API_fonctions.py
+APIfonctions.py
 A files that will contain all fonction that interact, modify and make direct use of the API and it's returned data:
-update():
-A fonction that make sure to update 'data.json' by requesting the data from the API with the corresponding URL, 
+update(URL -> str, name -> str):
+A fonction that make sure to update f'data_{name}.json' by requesting the data from the API with the corresponding URL, 
 convert the answer into json, updating the file with it and returning it.
 return data -> json
 """
 
-def update():
-    response=requests.get("https://portail-api-data.montpellier3m.fr/offstreetparking?limit=1000")
+def request_data(URL, name):
+    response=requests.get(URL)
     data = response.json() # Convert the response to JSON data
-    with open('data/data.json', 'w') as file:
+    with open(f"data/data_{name}.json", 'w') as file:
         json.dump(data, file, indent=4)
     return data
