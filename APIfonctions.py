de686b29c0c6f9acfa7bf9bf87ccbed71fe65372
Explosif3005 <75031787+Explosif3005@users.noreply.github.com>
2026-01-15 15:06:43 +0100

    Update APIfonctions.py
    Addition of fonctions for finding specific data from bike and parking

--- APIfonctions.py
+++ APIfonctions.py
@@ -14,3 +14,58 @@ def request_data(URL, name):
     data = response.json() # Convert the response to JSON data
     tfiles.writing_data(data, name)
     return data
+#------------------------------------------------------
+# Bellow are a number of fonctions made for this project to find the necessary data from the json
+# They are simple to modify and adapt dependanding on the data
+def find_parkings_names(data):
+    response = []
+    for i in range(len(data)):
+        response.append(data[i]['name']['value'])
+    return response
+
+def find_parking_occupation(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return (data[i]['totalSpotNumber']['value'] - data[i]['availableSpotNumber']['value'])
+
+def find_parking_total_place(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['totalSpotNumber']['value']
+
+def find_parking_available_place(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['availableSpotNumber']['value']
+
+def find_parking_state(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['status']['value']
+#-------------------------------------------
+def find_bike_names(data):
+    response = []
+    for i in range(len(data)):
+        response.append(data[i]['address']['value']['streetAddress'])
+    return response
+
+def find_bike_occupation(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['freeSlotNumber']['value']
+
+def find_bike_total_place(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['totalSlotNumber']['value']
+
+def find_bike_available_place(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['availableBikeNumber']['value']
+
+def find_bike_state(data, name):
+    for i in range(len(data)):
+        if name == data[i]['name']['value']:
+            return data[i]['status']['value']
+    
